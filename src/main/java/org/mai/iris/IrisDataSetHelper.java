04eff93d184be791ca98724be67473019275285a
Laiser399 <Laiser399@mail.ru>
2021-10-05 18:41:07 +0300

    helper

--- src/main/java/org/mai/iris/IrisDataSetHelper.java
+++ src/main/java/org/mai/iris/IrisDataSetHelper.java
@@ -1,36 +1,52 @@
 package org.mai.iris;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
 
 public class IrisDataSetHelper {
 
-    private List<Iris> dataSet;
+    private final List<Iris> dataSet;
 
     public IrisDataSetHelper(List<Iris> dataSet) {
         this.dataSet = dataSet;
     }
 
     public Double getAverage(ToDoubleFunction<Iris> func) {
-        throw new RuntimeException();
+        return dataSet.stream()
+                .mapToDouble(func)
+                .average()
+                .orElse(0);
     }
 
     public List<Iris> filter(Predicate<Iris> predicate) {
-        throw new RuntimeException();
+        return dataSet.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 
     public Double getAverageWithFilter(Predicate<Iris> filter, ToDoubleFunction<Iris> mapFunction) {
-        throw new RuntimeException();
+        return dataSet.stream()
+                .filter(filter)
+                .mapToDouble(mapFunction)
+                .average()
+                .orElse(0);
     }
 
-    public Map groupBy(Function groupFunction) {
-        throw new RuntimeException();
+    public <T> Map<T, List<Iris>> groupBy(Function<Iris, T> groupFunction) {
+        return dataSet.stream()
+                .collect(Collectors.groupingBy(groupFunction));
     }
 
-    public Map maxFromGroupedBy(Function groupFunction, ToDoubleFunction<Iris> obtainMaximisationValueFunction) {
-        throw new RuntimeException();
+    public <T> Map<T, Optional<Iris>> maxFromGroupedBy(Function<Iris, T> groupFunction,
+                                                       ToDoubleFunction<Iris> obtainMaximisationValueFunction) {
+        return dataSet.stream()
+                .collect(Collectors.groupingBy(groupFunction,
+                        Collectors.maxBy(Comparator.comparingDouble(obtainMaximisationValueFunction))));
     }
 }
